2ec8d59142a41a3539a4b87428ddc2956a4e1bc4
Amos Guetta <Amos.guetta.1990@gmail.com>
2018-11-27 20:25:05 +0200

    Initial express server with route file

--- server.js
+++ server.js
@@ -1,14 +1,36 @@
 //lsof -i :5000 ==> kill -9 <pid>
 //npm run server -> nodemon running.
 
-const express = require("express");
+const express = require("express"),
+  mongoose = require("mongoose");
 
-const port = process.env.PORT || 5000;
+const users = require("./routes/api/users");
+const profile = require("./routes/api/profile");
+const posts = require("./routes/api/posts");
 const app = express();
+
+//DB config
+const db = require("./config/keys").mongoURL;
+
+//Connect to MongoDB
+mongoose
+  .connect(
+    db,
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log("mongoDB Connected"))
+  .catch(err => console.log("Error from mongoose connect :", err));
 
 app.get("/", (req, res) => {
   res.send("Hello from ");
 });
+
+//User Routes
+
+app.use("/api/users", users);
+app.use("/api/profile", profile);
+app.use("/api/posts", posts);
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log("Server running on port:", port);
