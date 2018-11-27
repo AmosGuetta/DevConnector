901a4ac8739d4a5b95c5481938f7631008530bff
Amos Guetta <Amos.guetta.1990@gmail.com>
2018-11-27 22:37:33 +0200

    Login user component

--- routes/api/users.js
+++ routes/api/users.js
@@ -12,9 +12,9 @@ const User = require("../../models/User");
 // @access      Public
 router.get("/test", (req, res) => res.json({ msg: "Users Works" }));
 
-// @route GET api/users/register
-// @desc      Register user
-// @access    Public
+// @route POST  api/users/register
+// @desc        Register user
+// @access      Public
 router.post("/register", (req, res) => {
   User.findOne({ email: req.body.email }).then(user => {
     if (user) {
@@ -44,6 +44,31 @@ router.post("/register", (req, res) => {
         });
       });
     }
+  });
+});
+
+// @route POST  api/users/login
+// @desc        Login user / Returning JWT Token
+// @access      Public
+router.post("/login", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  // Find user by email
+  User.findOne({ email: email }).then(user => {
+    // Check for user
+    if (!user) {
+      res.status(404).json({ email: "User not found" });
+    }
+
+    // Check password
+    bcrypt.compare(password, user.password).then(isMatch => {
+      if (isMatch) {
+        res.json({ msg: "Success" });
+      } else {
+        return res.status(400).json({ password: "Password incorrect" });
+      }
+    });
   });
 });
 
