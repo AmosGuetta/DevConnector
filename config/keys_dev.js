0dde69142fd21b8f467672c78e37b7a108d1957c
Amos Guetta <amos.guetta.1990@gmail.com>
2018-12-06 18:41:11 +0200

    update keys

--- config/keys_dev.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  mongoURL:
-    // 'mongodb://AmosGuetta:Am0S1712@ds119024.mlab.com:19024/developerconnector',
-    'mongodb://amos:amos@devconnectordb-shard-00-00-gzoad.mongodb.net:27017,devconnectordb-shard-00-01-gzoad.mongodb.net:27017,devconnectordb-shard-00-02-gzoad.mongodb.net:27017/test?ssl=true&replicaSet=DevConnectorDB-shard-0&authSource=admin&retryWrites=true',
-  secretOrKey: 'secret'
-};
